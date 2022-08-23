6966f4af267acdb7c61d40e633dbfca62341c5fe
KOOHYO <zpfhzpfh112@naver.com>
2022-08-23 16:46:56 +0900

    QNA매퍼 완성

---
+++ src/test/java/com/ko/home/board/qna/QnaDAOTest.java
@@ -0,0 +1,14 @@
+package com.ko.home.board.qna;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class QnaDAOTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
