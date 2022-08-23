bc49c6b8a3c2b55b42a4bed8036aa844bef0e638
KOOHYO <zpfhzpfh112@naver.com>
2022-08-23 10:26:14 +0900

    최종 에러수정 완료!

--- src/main/java/com/ko/home/bankmembers/BankMembersController.java
+++ src/main/java/com/ko/home/bankmembers/BankMembersController.java
@@ -32,19 +32,6 @@ public class BankMembersController {
 		return mv;
 	}
 	
-	@RequestMapping(value = "myPage.iu", method = RequestMethod.GET)
-	public ModelAndView myPage(HttpSession session)throws Exception{
-		ModelAndView mv = new ModelAndView();
-		BankMembersDTO bankMembersDTO = (BankMembersDTO) session.getAttribute("member");
-		
-		bankMembersDTO = bankMembersService.getMyPage(bankMembersDTO);
-		
-		mv.setViewName("member/myPage");
-		mv.addObject("dto", bankMembersDTO);
-		
-		return mv;
-	}
-	
 	//로그인
 	@RequestMapping(value = "login.iu", method = RequestMethod.GET)
 	public String login()throws Exception{
