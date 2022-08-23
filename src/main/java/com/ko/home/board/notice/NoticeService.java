ffc3d92aebdb46816c4d9cf258cb4f64edf2cc94
KOOHYO <zpfhzpfh112@naver.com>
2022-08-23 15:11:42 +0900

    노티스 서비스 완성!

--- src/main/java/com/ko/home/board/notice/NoticeService.java
+++ src/main/java/com/ko/home/board/notice/NoticeService.java
@@ -2,6 +2,7 @@ package com.ko.home.board.notice;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ko.home.board.impl.BoardDTO;
@@ -10,34 +11,32 @@ import com.ko.home.board.impl.BoardService;
 @Service
 public class NoticeService implements BoardService{
 
+	@Autowired
+	private NoticeDAO noticeDAO;
+	
 	@Override
 	public List<BoardDTO> getList() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return noticeDAO.getList();
 	}
 
 	@Override
 	public BoardDTO getDetail(BoardDTO boardDTO) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return noticeDAO.getDetail(boardDTO);
 	}
 
 	@Override
 	public int setAdd(BoardDTO boardDTO) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		return noticeDAO.setAdd(boardDTO);
 	}
 
 	@Override
 	public int setUpdate(BoardDTO boardDTO) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		return noticeDAO.setUpdate(boardDTO);
 	}
 
 	@Override
 	public int setDelete(BoardDTO boardDTO) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		return noticeDAO.setDelete(boardDTO);
 	}
 
 }
