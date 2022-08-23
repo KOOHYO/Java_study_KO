92c5238d0499229f5fb64a6067aad71dd338f7a9
KOOHYO <zpfhzpfh112@naver.com>
2022-08-23 11:24:16 +0900

    매퍼연결!

--- src/main/java/com/ko/home/board/notice/NoticeDAO.java
+++ src/main/java/com/ko/home/board/notice/NoticeDAO.java
@@ -2,11 +2,20 @@ package com.ko.home.board.notice;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.ko.home.board.impl.BoardDAO;
 import com.ko.home.board.impl.BoardDTO;
 
+@Repository
 public class NoticeDAO implements BoardDAO {
 
+	@Autowired
+	private SqlSession sqlSession;
+	private final String NAMESPACE="com.ko.home.board.notice.NoticeDAO.";
+	
 	@Override
 	public List<BoardDTO> getList() throws Exception {
 		// TODO Auto-generated method stub
