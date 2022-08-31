22f7b1d158b1fcac3a535461cc512303f5aba9c8
KOOHYO <zpfhzpfh112@naver.com>
2022-08-31 17:50:53 +0900

    회원가입 체크

--- src/main/webapp/WEB-INF/views/member/join.jsp
+++ src/main/webapp/WEB-INF/views/member/join.jsp
@@ -12,29 +12,35 @@
 	<section class="container-fluid con-lg-7">
 		<div class="row">
 			
-			<form action="./join.iu" method="POST" enctype="multipart/form-data">
+			<form action="./join.iu" method="POST" enctype="multipart/form-data" id="joinform">
 				<h1>Join Page</h1>
 				<h2>회원가입</h2>
 		   		<div>
 		        	아이디를 입력하세요<br>
-		        	<input type="text" name="userName"><input type="button" value="중복확인"><br>
+		        	<input type="text" name="userName" id="id"><button type="button" id="idbtn">비밀번호 확인</button><br>
+					<div id="d1"></div>
 		       		비밀번호를 입력 하세요<br>
-		        	<input type="password"><br> 
+		        	<input type="password" id="pw"><br>
+					<div id="d2"></div>
 		        	비밀번호를 다시 입력하세요<br>
-		        	<input type="password" name="passWord"><input type="button" value="비밀번호 확인"><br>
+		        	<input type="password" name="passWord" id="pw2"><button type="button" id="pwbtn">비밀번호 확인</button><br>
+					<div id="d3"></div>
 		    	</div>
 		    	<br>
 		    	<div>
 			       이름을 입력하세요<br>
-			       <input type="text" name="name"><br>
+			       <input type="text" name="name" id="name"><br>
+				   <div id="d4"></div>
 			       이메일을 입력하세요<br>
-			       <input type="email" name="email"><br>
+			       <input type="email" name="email" id="email"><br>
+				   <div id="d5"></div>
 			       전화번호를 입력하세요<br>
-			       <input type="text" name="phone"><br>
+			       <input type="text" name="phone" id="phone"><br>
+				   <div id="d6"></div>
 			       <br>
 			       <hr>
 			       <br>
-			       <input type="submit" value="회원가입완료"><input type="button" value="회원가입취소">
+				   <button type="button" id="joinbtn">회원가입 완료</button><button type="button">회원가입 취소</button>
 			   </div>
 			   <div>
 			   		Photo<br>
@@ -47,6 +53,9 @@
 	</section>
 	
 	<c:import url="../template/footer.jsp"></c:import>
-
+	<script src="/resources/js/member.js"></script>
+	<script>
+		joinCheck();
+	</script>
 </body>
 </html>
--- src/main/webapp/resources/js/member.js
+++ src/main/webapp/resources/js/member.js
@@ -83,3 +83,121 @@ function check(){
         // }
     });
 }
+
+function joinCheck(){
+    //선택자
+    const joinform = document.getElementById("joinform");
+    const joinbtn = document.getElementById("joinbtn");
+
+    //Check 결과
+    let idCheck=false;
+    let pwCheck=false;
+    let pw2Check=false;
+    let nameCheck=false;
+    let emailCheck=false;
+    let phoneCheck=false;
+
+    const id = document.getElementById("id");
+    const pw = document.getElementById("pw");
+    const pw2 = document.getElementById("pw2");
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const phone = document.getElementById("phone");
+
+    const d1 = document.getElementById("d1");
+    const d2 = document.getElementById("d2");
+    const d3 = document.getElementById("d3");
+    const d4 = document.getElementById("d4");
+    const d5 = document.getElementById("d5");
+    const d6 = document.getElementById("d6");
+
+    const idbtn = document.getElementById("idbtn");
+    const pwbtn = document.getElementById("pwbtn");
+
+    id.addEventListener("blur", function(){
+        if(id.value.length>1){
+            idCheck=true;
+             // id.focus();
+            d1.innerHTML="";
+        }else {
+            idCheck=false;
+            d1.innerHTML="아이디는 2글자 이상 입력하세요";
+        }
+
+    });
+
+    // 1. pw 길이 체크
+    pw.addEventListener("keyup", function(){
+        if(v2.value.length>5){
+            pwCheck=true;
+            // id.focus();
+            d2.innerHTML="";
+        }else {
+            pwCheck=false;
+            d2.innerHTML="비밀번호는 6글자 이상 입력하세요";
+        }
+
+    });
+
+    // 2. pw가 같은지 체크
+    pw2.addEventListener("");
+
+
+    pwbtn.addEventListener("blur", function(){
+        
+        if(pw.value==pw2.value){
+            pw2Check=true;
+            d3.innerHTML="";
+        }else {
+            pw2Check=false;
+            d3.value="";
+            d3.innerHTML="불일치합니다 다시입력해주세요";
+        }
+
+    });
+
+    name.addEventListener("keyup", function(){
+        let name2 = name.value;
+        let t3 = "";
+        for(let i=0; i<name2.length; i++){
+            if(name.length==0){
+                 console.log(name2.length);
+                 t3="1글자 이상 이름을 입력하세요"
+                 d3.innerHTML=t3;
+                 break;
+            }
+        }
+    });
+
+    joinbtn.addEventListener("click", function(){
+       let name2 = name.value;
+       let email2 = email.value;
+       let phone2 = phone.value;
+
+       let t3 = "";
+       for(let i=0; i<name2.length; i++){
+           console.log(name2.length);
+           if(name.value==""){
+                t3="1글자 이상 이름을 입력하세요"
+                d3.innerHTML=t3;
+                break;
+           }
+       }
+
+       for(let i=0; i<email2.length; i++){
+            if(email2.length=0){
+                t3="1글자 이상 이메일을 입력하세요"
+                d3.innerHTML=t3;
+            }
+        }
+
+        for(let i=0; i<phone2.length; i++){
+            if(phone2.length=0){
+                t3="1글자 이상 전화번호를 입력하세요"
+                d3.innerHTML=t3;
+            }
+        }
+
+    });
+
+}
