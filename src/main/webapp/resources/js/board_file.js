eccc399f6ac9d733b70f3a8e2acdfca86886497d
KOOHYO <zpfhzpfh112@naver.com>
2022-09-02 09:57:19 +0900

    첨부파일 추가란 삭제 완성

--- src/main/webapp/resources/js/board_file.js
+++ src/main/webapp/resources/js/board_file.js
@@ -86,14 +86,11 @@ fileAdd.addEventListener("click", function(){
 addFiles.addEventListener("click", function(event){
     //div id file1
     //button title="1"
-    if(event.target.classList[0]=='del'){
+    let button = event.target;
+    if(button.classList[0]=='del'){
         alert("삭제 하시겠습니까?");
         // console.log(event.target.title)
-        for(let i=0; i<idx; i++){
-            if(event.target.title==i){
-                fileAdd.div.remove();
-            }
-
-        }
+        document.getElementById("file"+button.title).remove();
+        count--;
     }
 });
