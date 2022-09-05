6f729d275a45fe946f3d72dc8ac97ed42ab1c42a
KOOHYO <zpfhzpfh112@naver.com>
2022-09-05 15:57:14 +0900

    commentDAO 완성

--- src/main/java/com/ko/home/BankBook/BankBookCommentDAO.java
+++ src/main/java/com/ko/home/BankBook/BankBookCommentDAO.java
@@ -1,5 +1,36 @@
 package com.ko.home.BankBook;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class BankBookCommentDAO {
 
+	@Autowired
+	private SqlSession sqlSession;
+	private final String NAMESPACE="com.ko.home.BankBook.BankBookCommentDAO.";
+	
+	public int setAdd(BankBookCommentDTO bankBookCommentDTO)throws Exception{
+		
+		return sqlSession.insert(NAMESPACE+"setAdd", bankBookCommentDTO);
+	}
+	
+	public List<BankBookCommentDTO> getList(BankBookCommentDTO bankBookCommentDTO)throws Exception{
+		
+		return sqlSession.selectList(NAMESPACE+"getList", bankBookCommentDTO);
+	}
+	
+	public int setUpdate(BankBookCommentDTO bankBookCommentDTO)throws Exception{
+		
+		return sqlSession.update(NAMESPACE+"setUpdate", bankBookCommentDTO);
+	}
+	
+	public int setDelete(BankBookCommentDTO bankBookCommentDTO)throws Exception{
+		
+		return sqlSession.delete(NAMESPACE+"setDelete", bankBookCommentDTO);
+	}
+	
 }
